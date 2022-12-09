f72adfaaff3d945dc4f4cd4b12987a5ad0731d08
nick-echevarria <nick.echev@gmail.com>
2022-12-09 14:15:51 -0500

    minor: implemented SASS

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,40 @@
-import './App.css';
+import './styles/_app.scss';
 
 const App = () => {
   const categories = [
     {
       id: 1,
-      title: 'Hats',
+      title: 'Hats'
     },
     {
       id: 1,
-      title: 'Jackets',
+      title: 'Jackets'
     },
     {
       id: 1,
-      title: 'Sneakers',
+      title: 'Sneakers'
     },
     {
       id: 1,
-      title: 'Womens',
+      title: 'Womens'
     },
     {
       id: 1,
-      title: 'Mens',
-    },
+      title: 'Mens'
+    }
   ];
   return (
-    <div className='categories-container'>
-      {categories.map(({ title }) => (
-        <div className='category-container'>
-          <div className='background-image'></div>
-          <div className='category-body-container'>
+    <div className="categories-container">
+      {categories.map(({ title, id }) => (
+        <div key={id} className="category-container">
+          <div className="background-image"></div>
+          <div className="category-body-container">
             <h2>{title}</h2>
             <h2>Shop Now</h2>
           </div>
         </div>
       ))}
     </div>
-    
   );
 };
 
