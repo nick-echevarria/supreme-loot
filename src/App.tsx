6222bfde8a0bdd58bda0e0bd448cb6869b306395
nick-echevarria <nick.echev@gmail.com>
2022-12-09 11:06:20 -0500

    intitial commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import './App.css';
+
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 1,
+      title: 'Jackets',
+    },
+    {
+      id: 1,
+      title: 'Sneakers',
+    },
+    {
+      id: 1,
+      title: 'Womens',
+    },
+    {
+      id: 1,
+      title: 'Mens',
+    },
+  ];
+  return (
+    <div className='categories-container'>
+      {categories.map(({ title }) => (
+        <div className='category-container'>
+          <div className='background-image'></div>
+          <div className='category-body-container'>
+            <h2>{title}</h2>
+            <h2>Shop Now</h2>
+          </div>
+        </div>
+      ))}
+    </div>
+    
+  );
+};
+
+export default App;
