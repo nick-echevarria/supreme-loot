aca82605d4147a49d9befc10bc1e9ed1704ef991
nick-echevarria <nick.echev@gmail.com>
2022-12-22 10:46:16 +0100

    feat: added reselect library + refactored category selector to make use of it

--- src/stores/redux/categories/categories.selector.js
+++ src/stores/redux/categories/categories.selector.js
@@ -1,6 +1,20 @@
-export const selectCategoriesMap = (state) =>
-  state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from 'reselect';
+
+//input selector
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+//as long as the categories array does not change, do not run the reduce method below
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
