183abb2f8779ffd39c13412e408361a698b48ea3
nick-echevarria <nick.echev@gmail.com>
2022-12-22 16:26:12 +0100

    feat: added redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
-import { store } from './store/redux/store';
+import { store, persistor } from './store/redux/store';
 
 import './styles/application.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
