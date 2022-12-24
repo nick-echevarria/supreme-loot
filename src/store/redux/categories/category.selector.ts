08b5fb10bdcb0f6c170381f909d246b758d25984
nick-echevarria <nick.echev@gmail.com>
2022-12-24 22:02:06 +0100

    minor: typed some of reducer, some actions

--- src/store/redux/categories/category.selector.ts
+++ src/store/redux/categories/category.selector.ts
@@ -1,7 +1,10 @@
 import { createSelector } from 'reselect';
 
+import { CategoriesState } from './category.reducer';
+import { CategoryMap } from './category.types';
+
 //input selector
-const selectCategoryReducer = (state) => state.categories;
+const selectCategoryReducer = (state): CategoriesState => state.categories;
 
 export const selectCategories = createSelector(
   [selectCategoryReducer],
@@ -11,12 +14,12 @@ export const selectCategories = createSelector(
 //as long as the categories array does not change, do not run the reduce method below
 export const selectCategoriesMap = createSelector(
   [selectCategories],
-  (categories) =>
+  (categories): CategoryMap =>
     categories.reduce((acc, category) => {
       const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
-    }, {})
+    }, {} as CategoryMap)
 );
 
 export const selectCategoriesIsLoading = createSelector(
---
+++ src/utils/reducer/reducer.utils.ts
@@ -0,0 +1,26 @@
+import { AnyAction } from 'redux';
+
+export type ActionWithPayload<T, P> = {
+  type: T;
+  payload: P;
+};
+
+export type Action<T> = {
+  type: T;
+};
+
+//use function overloading to create multiple type defs with the same name
+//like this, a function will be able to receive diff parameters and return appropriate type
+export function createAction<T extends string, P>(
+  type: T,
+  payload: P
+): ActionWithPayload<T, P>;
+
+export function createAction<T extends string>(
+  type: T,
+  payload: void
+): Action<T>;
+
+export function createAction<T extends string, P>(type: T, payload: P) {
+  return { type, payload };
+}
