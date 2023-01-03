1c177801f7bbbbbc5dd9e16fb4fe9c0a0a8ee470
nick-echevarria <nick.echev@gmail.com>
2023-01-03 14:50:56 +0100

    feat: finished migrating thunks + other asynch code to sagas

--- src/routes/navigation/navigation.tsx
+++ src/routes/navigation/navigation.tsx
@@ -1,19 +1,22 @@
 import { Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import CartIcon from '../../components/cart-icon/cart-icon';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown';
 
 import { selectIsCartOpen } from '../../store/redux/cart/cart.selector';
 import { selectCurrentUser } from '../../store/redux/user/user.selector';
+import { signOutStart } from '../../store/redux/user/user.actions';
 
 import { ReactComponent as SupremeLootLogo } from '../../assets/supreme-loot-logo.svg';
-import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 const Navigation: React.FC = () => {
+  const dispatch = useDispatch();
   const currentUser = useSelector(selectCurrentUser);
   const isCartOpen = useSelector(selectIsCartOpen);
+
+  const signOutUser = () => dispatch(signOutStart());
 
   return (
     <Fragment>
