dba61491d07e458d83c730d55c871e304ab4b73f
nick-echevarria <nick.echev@gmail.com>
2023-01-10 20:22:21 -0500

    feat: fixed navigation from directory by adding route to categories

--- src/models/category.ts
+++ src/models/category.ts
@@ -2,4 +2,5 @@ export type CategoryType = {
   id: number;
   title: string;
   imageUrl: string;
+  route: string;
 };
--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -6,28 +6,33 @@ const Home: React.FC = () => {
   const categories: CategoryType[] = [
     {
       id: 1,
-      title: 'Hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png'
+      title: 'hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      route: 'shop/hats'
     },
     {
       id: 2,
-      title: 'Jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png'
+      title: 'jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      route: 'shop/jackets'
     },
     {
       id: 3,
-      title: 'Sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png'
+      title: 'sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      route: 'shop/sneakers'
     },
     {
       id: 4,
-      title: 'Womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png'
+      title: 'womens',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      route: 'shop/womens'
     },
     {
       id: 5,
-      title: 'Mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png'
+      title: 'mens',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      route: 'shop/mens'
     }
   ];
   return (
